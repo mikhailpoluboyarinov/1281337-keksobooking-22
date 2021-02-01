8c3586a72a5989c1d37776322038f7b487aa67b8
Mikhail <mikhailpoluboyarinov@yandex.ru>
2021-02-01 21:45:24 +0300

    Поправил функции

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ getRandomNumber();
 const getRandomLocation = (min,max,digits) => {
   if (min > max && max >=0 && min > 0) {
     return +(Math.random() * (min - max) + max).toFixed(digits);
-  } else  if (min > 0 && max > 0) {
+  } else  if (min >= 0 && max > 0) {
     return +(Math.random() * (max - min) + min).toFixed(digits);
   }
   return false;
