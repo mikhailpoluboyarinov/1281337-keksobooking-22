d213f2eacbe9da4fb80fca95e2a38726bad14e73
Mikhail <mikhailpoluboyarinov@yandex.ru>
2021-02-01 21:42:50 +0300

    Поправил функции

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const getRandomNumber = (min, max) => {
-  if (min > 0 && min < max) {
+  if (min > max && max >=0 && min > 0) {
+    return Math.floor(Math.random() * (min - max + 1)) + max;
+  } else  if (min >= 0 && max > 0) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
   return false;
@@ -10,7 +12,9 @@ getRandomNumber();
 
 
 const getRandomLocation = (min,max,digits) => {
-  if (min > 0 && min < max) {
+  if (min > max && max >=0 && min > 0) {
+    return +(Math.random() * (min - max) + max).toFixed(digits);
+  } else  if (min > 0 && max > 0) {
     return +(Math.random() * (max - min) + min).toFixed(digits);
   }
   return false;
